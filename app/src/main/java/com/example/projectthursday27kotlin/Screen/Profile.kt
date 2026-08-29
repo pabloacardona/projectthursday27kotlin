fccfe3c6480e9b05db54c6889e04d0d0c810af2a
Juan Pablo Arroyave Cardona <speedtester666@gmail.com>
2026-08-28 23:56:23 -0500

    Changed screens backgrounds pastel colors

--- app/src/main/java/com/example/projectthursday27kotlin/Screen/Profile.kt
+++ app/src/main/java/com/example/projectthursday27kotlin/Screen/Profile.kt
@@ -1,5 +1,6 @@
 package com.example.projectthursday27kotlin.Screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,11 +9,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.projectthursday27kotlin.ui.theme.PeachPastel
 
 @Composable
 fun ProfileScreen() {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(PeachPastel),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
--- app/src/main/java/com/example/projectthursday27kotlin/ui/theme/Color.kt
+++ app/src/main/java/com/example/projectthursday27kotlin/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LavenderPastel = Color(0xFFE6E6FA)
+val MintPastel = Color(0xFFB2E5D0)
+val PeachPastel = Color(0xFFFFDAB9)
+val SkyPastel = Color(0xFFAECFEB)
